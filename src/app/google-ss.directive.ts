d7138480d29f6461e831c84e465a6db84cee3ff1
mkerekes1427 <73243252+mkerekes1427@users.noreply.github.com>
2024-09-16 14:03:04 -0400

    Update google-ss.directive.ts

--- src/app/google-ss.directive.ts
+++ src/app/google-ss.directive.ts
@@ -3,7 +3,7 @@ import { Directive, HostListener } from "@angular/core";
 import { AngularFireAuth } from "@angular/fire/compat/auth";
 import { GoogleAuthProvider } from "@firebase/auth";
 
-// The following site shows authentication
+// The following site shows authentication. Also, Firebase Gemini helped with debugging.
 // https://medium.com/@gabriel.cournelle/firebase-authentication-in-angular-ab1b66d041dc
 
 @Directive({
