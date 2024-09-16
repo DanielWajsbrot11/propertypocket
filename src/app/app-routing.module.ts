48bfa287dfc588b960304e1e19b0dfad7475284b
mkerekes1427 <73243252+mkerekes1427@users.noreply.github.com>
2024-09-16 13:40:15 -0400

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,9 @@ import { RouterModule, Routes } from '@angular/router';
 import { NgxInfiniteScrollComponent } from './components/ngx-infinite-scroll/ngx-infinite-scroll.component';
 import { authGuard } from './auth.guard';
 
+// The following site shows authentication and routing
+// https://medium.com/@gabriel.cournelle/firebase-authentication-in-angular-ab1b66d041dc
+
 const routes: Routes = [
   {
     path: 'home',
@@ -10,9 +13,6 @@ const routes: Routes = [
     canActivate: [authGuard],
   },
 ];
-
-// The following site shows authentication
-// https://medium.com/@gabriel.cournelle/firebase-authentication-in-angular-ab1b66d041dc
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
