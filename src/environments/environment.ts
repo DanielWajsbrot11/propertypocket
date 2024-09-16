d9bd0abe5c454da0513398f923fe0a19af26efed
mkerekes1427 <73243252+mkerekes1427@users.noreply.github.com>
2024-09-16 19:05:59 -0400

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,6 @@
 
+// This file contains the Firebase configuration object, which is used in deployment according to Chat-GPT.
+
 // The following site shows authentication
 // https://medium.com/@gabriel.cournelle/firebase-authentication-in-angular-ab1b66d041dc
 
