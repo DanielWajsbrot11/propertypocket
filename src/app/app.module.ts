00eab44ddd90cdc6d5574559015570825d1b58ce
Daniel <daniel.wajsbrot038@gmail.com>
2024-09-16 15:22:12 -0400

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,7 +16,6 @@ import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 import { PropertyCardComponent } from './components/property-card/property-card.component';
 import { LandingComponent } from './components/landing/landing.component';
 
-
 // The following site shows authentication
 // https://medium.com/@gabriel.cournelle/firebase-authentication-in-angular-ab1b66d041dc
 
