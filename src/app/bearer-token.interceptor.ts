315b866b18280df0fa5c4a270511500e84ad0728
mkerekes1427 <73243252+mkerekes1427@users.noreply.github.com>
2024-09-18 00:24:59 -0400

    Update bearer-token.interceptor.ts

--- src/app/bearer-token.interceptor.ts
+++ src/app/bearer-token.interceptor.ts
@@ -15,8 +15,6 @@ import { AngularFireAuth } from "@angular/fire/compat/auth";
 import { from, lastValueFrom, Observable } from "rxjs";
 import { environment } from "../environments/environment";
 
-
-// needs to add this function because getting the token is async
 const addBearerToken = async (
   req: HttpRequest<any>,
   next: HttpHandlerFn,
@@ -32,8 +30,6 @@ const addBearerToken = async (
   return lastValueFrom(next(req));
 };
 export const bearerTokenInterceptor: HttpInterceptorFn = (req, next) => {
-  // only add the bearer token to requests to the backend
-  // Note that you can customize it to only add the bearer token to certain requests
   if (req.url.startsWith(environment.backendUrl)) {
     return from(addBearerToken(req, next));
   } else {
