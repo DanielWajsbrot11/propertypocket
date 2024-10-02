7b23cbe3151922596218aea529557fc0201f575f
David-J-Rogers <143656714+David-J-Rogers@users.noreply.github.com>
2024-10-01 21:37:16 -0400

    Added user like and bookmark buttons to /saved

--- src/app/components/bookmarked-listings/bookmarked-listings.component.ts
+++ src/app/components/bookmarked-listings/bookmarked-listings.component.ts
@@ -1,10 +1,43 @@
 import { Component } from '@angular/core';
+import { BackendService } from '../../services/backend.service';
 
 @Component({
   selector: 'app-bookmarked-listings',
   templateUrl: './bookmarked-listings.component.html',
-  styleUrl: './bookmarked-listings.component.scss'
+  styleUrls: ['./bookmarked-listings.component.scss']
 })
 export class BookmarkedListingsComponent {
 
+  bookmarks: any[] = []; 
+  likes: any[] = []; 
+
+  constructor(private backendService: BackendService) {}
+
+  logBookmarkedZPIDs() {
+    this.backendService.getBookmarks().then(bookmarks => {
+      if (bookmarks) {
+        this.bookmarks = bookmarks; 
+        const zpids = this.bookmarks.map(bookmark => bookmark.zpid);
+        console.log('ZPIDs (Bookmarks):', zpids);
+      } else {
+        console.log('No bookmarks found');
+      }
+    }).catch(error => {
+      console.error('Error fetching bookmarks:', error);
+    });
+  }
+
+  logLikedZPIDs() {
+    this.backendService.getLikes().then(likes => {
+      if (likes) {
+        this.likes = likes;
+        const zpids = this.likes.map(like => like.zpid);
+        console.log('ZPIDs (Likes):', zpids);
+      } else {
+        console.log('No likes found');
+      }
+    }).catch(error => {
+      console.error('Error fetching likes:', error);
+    });
+  }
 }
