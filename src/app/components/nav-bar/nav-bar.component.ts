a989252ad336ca02c94708c482000023eccac4f3
Joe Crider <jcrider@Admins-MacBook-Pro.local>
2024-10-03 22:31:12 -0400

    routing

--- src/app/components/nav-bar/nav-bar.component.ts
+++ src/app/components/nav-bar/nav-bar.component.ts
@@ -3,6 +3,8 @@ import { AngularFireAuth } from "@angular/fire/compat/auth";
 import { ZipRetrieval } from '../../services/zipRetrieval.service';
 import { SavesRetrieval } from '../../services/savesRetrieval.service';
 import { BackendService } from '../../services/backend.service';
+import { NavigationEnd, Router } from '@angular/router';
+import { filter } from 'rxjs';
 
 
 @Component({
@@ -15,6 +17,7 @@ export class NavBarComponent {
 
   zip: string = "";
   username: string | null = "Sign in";
+  showZip: string = "block";
   
   isMenuOpen = false;
 
@@ -23,6 +26,15 @@ export class NavBarComponent {
   }
 
   async ngOnInit() {
+
+    // Chat-GPT for getting current url.
+    this.router.events
+      .pipe(filter((event: any) => event instanceof NavigationEnd))
+      .subscribe(() => {
+        if (this.router.url === '/saved') {
+          this.showZip = "hidden";
+        }
+      });
 
     // Chat-GPT for accessing user and name.
     this.angularFireAuth.authState.subscribe(user => {
@@ -36,6 +48,7 @@ export class NavBarComponent {
   // Chat-GPT for handling keyboard enter event and service call with state managemnt.
   // State manangement with likes and bookmarks copies same logic from zipRetrieval service in infinite scroll. See reference there.
   async submitZip(){
+
     this.zipRetrieval.updateZip(this.zip);
 
     const user = await this.angularFireAuth.currentUser;
@@ -68,7 +81,7 @@ export class NavBarComponent {
   // The following site shows authentication with signing out. 
   // https://medium.com/@gabriel.cournelle/firebase-authentication-in-angular-ab1b66d041dc
   constructor(public angularFireAuth: AngularFireAuth, private zipRetrieval: ZipRetrieval, 
-    private savesRetrieval: SavesRetrieval, private backendService: BackendService
+    private savesRetrieval: SavesRetrieval, private backendService: BackendService, private router: Router
   ) {}
 
 }
